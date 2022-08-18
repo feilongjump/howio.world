0cfc3325d81e36b7cdf606d327cf569490c37e71
feilongjump <feilongjump@qq.com>
2022-08-18 23:21:33 +0800

    feat: 登录页

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import NProgress from '@/utils/progress'
-import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect: { name: 'auth-login' }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('@/views/AboutView.vue'),
-      meta: {
-        title: 'About'
-      }
+      path: '/auth',
+      children: [
+        {
+          path: 'login',
+          name: 'auth-login',
+          component: () => import('@/views/auth/LoginView.vue')
+        }
+      ]
     }
   ]
 })
---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import Logo from '@/assets/logo.png'
+import { User, Lock } from '@element-plus/icons-vue'
+
+const params = reactive({
+  username: '',
+  password: ''
+})
+
+const onSubmit = () => {
+  console.info(params)
+}
+</script>
+
+<template>
+  <div class="auth-container">
+    <div class="auth-form-container">
+      <!-- header -->
+      <div class="header">
+        <img :src="Logo" />
+        <span>Welcome back 👏</span>
+      </div>
+      <p>失败往往是黎明前的黑暗<br />继之而出现的就是成功的朝霞 🌞</p>
+
+      <!-- form -->
+      <el-form label-position="top" :model="params" size="large">
+        <el-form-item label="Username">
+          <el-input v-model="params.username" placeholder="Please input username or email">
+            <template #prefix>
+              <el-icon><User /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="Password">
+          <el-input v-model="params.password" placeholder="Please input password" show-password>
+            <template #prefix>
+              <el-icon><Lock /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+
+        <!-- register or forget-password -->
+        <div class="auth-other">
+          <span>
+            <RouterLink to="/">没有账号</RouterLink>还是<RouterLink to="/">忘记了密码</RouterLink>
+          </span>
+        </div>
+        <el-form-item class="auth-form-btn-item">
+          <el-button type="primary" size="large" @click="onSubmit" plain>Login</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '@/assets/styles/auth.scss';
+</style>
