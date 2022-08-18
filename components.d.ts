cbe2944d87dfe0e24dbde9b8eb5fcbf5429775d0
feilongjump <feilongjump@qq.com>
2022-08-19 00:21:11 +0800

    chore: remove code

--- components.d.ts
+++ components.d.ts
@@ -10,7 +10,6 @@ declare module '@vue/runtime-core' {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
-    ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
     IEpLock: typeof import('~icons/ep/lock')['default']
     IEpUser: typeof import('~icons/ep/user')['default']
