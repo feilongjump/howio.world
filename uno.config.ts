dfdd2f2fc6dc259f694b3264ed132f624122e317
ifeilong <feilongjump@qq.com>
2024-02-05 16:20:18 +0800

    :lipstick: 更新样式文件

--- uno.config.ts
+++ uno.config.ts
@@ -2,6 +2,7 @@ import {
   defineConfig,
   presetAttributify,
   presetUno,
+  transformerDirectives,
 } from 'unocss'
 
 export default defineConfig({
@@ -10,5 +11,8 @@ export default defineConfig({
     presetUno(),
     // 支持attributify mode,简单说就是为了避免样式写太长难维护，能将 py-2 px-2这种相关属性整合起来写成 p="y-2 x-4"
     presetAttributify(),
+  ],
+  transformers: [
+    transformerDirectives(),
   ],
 })
