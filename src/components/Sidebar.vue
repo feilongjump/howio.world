59c617ab70819332ce255bbee0fb4706150ee649
ifeilong <feilongjump@qq.com>
2024-02-07 16:07:29 +0800

    :sparkles: layouts & sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const router = useRouter()
+const route = useRoute()
+
+const menus = []
+
+function go(routeName: any) {
+  router.push({ name: routeName })
+}
+</script>
+
+<template>
+  <n-layout-sider
+    :native-scrollbar="false"
+    collapse-mode="transform"
+    :collapsed-width="0"
+    :width="240"
+    show-trigger="bar"
+    class="bg-gray-100/30"
+  >
+    <div h-24 w-full flex items-center justify-center>
+      <Logo />
+    </div>
+    <div pl-8>
+      <div
+        v-for="menu in menus" :key="menu.name"
+        class="hover:bg-blue-500/70 hover:text-white"
+        my-4 cursor-pointer rounded-l-xl py-3 pl-8
+        :class="route.name === menu.routeName ? 'bg-blue-500/70 text-white font-bold' : ''"
+        @click="go(menu.routeName)"
+      >
+        {{ menu.name }}
+      </div>
+    </div>
+  </n-layout-sider>
+</template>
---
+++ src/layouts/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <n-layout min-h-screen>
+    <n-layout position="absolute" has-sider>
+      <Sidebar />
+      <n-layout :native-scrollbar="false">
+        <RouterView />
+      </n-layout>
+    </n-layout>
+  </n-layout>
+</template>
