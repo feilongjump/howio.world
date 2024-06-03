2754af0a0683960b0dc19b3102340567c801d772
ifeilong <feilongjump@qq.com>
2024-06-03 16:33:13 +0800

    :bricks: 后台布局方式更改

--- src/layouts/backstage.vue
+++ src/layouts/backstage.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
+import Simplebar from 'simplebar-vue'
 </script>
 
 <template>
   <div class="w-dvw min-h-dvh flex">
-    <div class="w-80 hidden sm:block h-dvh">
+    <div class="w-[--backstage-sidebar-w] hidden sm:block h-dvh fixed">
       <BackstageSidebar />
     </div>
-    <main class="flex-1 px-2 sm:px-6">
-      <BackstageHeader />
-      <RouterView />
+    <main class="w-full max-h-dvh overflow-hidden">
+      <Simplebar class="h-full">
+        <div class="mx-2 sm:mx-6 sm:pl-[--backstage-sidebar-w]">
+          <!-- Header -->
+          <div class="h-[--backstage-header-h]">
+            <BackstageHeader />
+          </div>
+          <!-- Main -->
+          <div>
+            <RouterView />
+          </div>
+        </div>
+      </Simplebar>
     </main>
   </div>
 </template>
