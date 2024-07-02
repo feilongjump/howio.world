cc21cc418572e15a9d2e9e166250397f608554d1
ifeilong <feilongjump@qq.com>
2024-07-02 11:14:31 +0800

    滚动条

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,17 @@
   import { onMount } from 'svelte'
   import { themeChange } from 'theme-change'
 
+  // Scrollbars
+  import 'simplebar'
+  import 'simplebar/dist/simplebar.css'
+  // You will need a ResizeObserver polyfill for browsers that don't support it! (iOS Safari, Edge, ...)
+  import ResizeObserver from 'resize-observer-polyfill'
+
   onMount(() => {
     // 主题切换
     themeChange(false)
+
+    window.ResizeObserver = ResizeObserver
   })
 </script>
 
@@ -14,15 +22,17 @@
   <Header />
 
   <main>
-    <slot />
+    <div class="h-full" data-simplebar data-simplebar-auto-hide="true">
+      <slot />
+    </div>
   </main>
 </div>
 
 <style>
   main {
     margin-top: var(--header-h);
-    min-height: calc(100dvh - var(--header-h));
+    max-height: calc(100dvh - var(--header-h));
     background-color: var(--main-bg-color);
-    @apply rounded-t-xl md:rounded-t-3xl;
+    @apply h-dvh overflow-hidden;
   }
 </style>
